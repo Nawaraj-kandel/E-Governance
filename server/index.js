d1b387f5e5db791a28759ffc5d6e62e4372f4f91
Nawaraj-kandel <kandeln309@gmail.com>
2024-04-25 09:59:15 +0545

    Setup of server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+
+app.listen(3000, () => console.log('Server running on port 3000'));
